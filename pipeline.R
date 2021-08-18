879ed5a563f5bf16f304c13bfe3ced7ff744a1c1
eaortiz <eao7r@virginia.edu>
2021-08-18 13:32:32 -0400

    added pipeline.R plotting

---
+++ pipeline.R
@@ -0,0 +1,12 @@
+# load tidyverse package
+library(tidyverse)
+
+# Part 1: Read data into data frame
+df <- read.csv("andre.csv")
+
+# Part 2: remove data from 1976 and after 1993
+df <- filter(df, Year != 1976)
+df <- filter(df, Year < 1994)
+
+# Part 3: make a histogram
+ggplot(data=df, aes(df$H)) + geom_histogram()
