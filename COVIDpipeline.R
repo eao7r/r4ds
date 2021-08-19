bd224fb3bd57544157ac766a079199cb3a02165f
eaortiz <eao7r@virginia.edu>
2021-08-19 09:22:14 -0400

    Covid pipeline

---
+++ COVIDpipeline.R
@@ -0,0 +1,14 @@
+# load tidyverse package
+library(tidyverse)
+
+# Part 1: Read data into data frame
+df <- read.csv("owid-covid-data.csv", header = TRUE)
+
+# Part 2: remove data from 1976 and after 1993
+df <- filter(df, location == "United States" & people_fully_vaccinated_per_hundred & hosp_patients_per_million )
+
+
+# Part 3: make a multiline plot
+ggplot(df, aes(date, group=1)) + 
+  geom_line(aes(y = hosp_patients_per_million), color = "darkred") +
+  geom_line(aes(y = people_fully_vaccinated_per_hundred), color="steelblue")
